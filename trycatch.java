ef7651e1e77440b67f2e83634e42328c9d52f547
malli24 <malleswari82006@gmail.com>
2024-12-14 15:52:20 +0530

    Add files via upload

---
+++ trycatch.java
@@ -0,0 +1,27 @@
+public class trycatch
+{
+    public static void main(String args[])
+    {
+        try
+        {
+            int d=90/0;
+        }
+        catch(ArithmeticException v)
+        {
+            System.out.println("Bad Request");
+            try
+            {
+                int d=90/0;
+            }
+            catch(ArithmeticException e)
+            {
+                System.out.println("catch block is running");
+            }
+            finally
+            {
+                System.out.println("Finally is running");
+            }
+            
+        }
+    }
+}
