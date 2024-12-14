41b2f010c1ff6a1a3b871dc564f6ad1247c3b1e4
malli24 <malleswari82006@gmail.com>
2024-12-14 13:42:56 +0530

    Add files via upload

---
+++ single.java
@@ -0,0 +1,23 @@
+ public class single
+    {
+        public static void main(String[]args){
+            b h=new b(); 
+            h.display();  
+            }
+    }
+    class a{
+    public void print()
+    {
+        System.out.println("Hii malli");
+    }
+    }
+    class b extends a
+    {
+        public void display()
+        {
+        System.out.println("hello anu");
+        }
+    }
+   
+
+
