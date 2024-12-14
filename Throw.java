8067db5f83df6dfe70d422a2efe3b31f7db90051
malli24 <malleswari82006@gmail.com>
2024-12-14 15:54:42 +0530

    Add files via upload

---
+++ Throw.java
@@ -0,0 +1,14 @@
+public class Throw
+{
+    public static void main(String args[])
+    {
+        try
+        {
+            throw new Exception("Error");
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+}
